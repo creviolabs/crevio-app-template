203c11b848331cd22e7659f3ea2c9b1cfc852407
Axel <73122370+axelgrubba@users.noreply.github.com>
2026-03-15 16:44:20 +0100

    feat: add back @crevio/sdk

--- app/lib/crevio.ts
+++
@@ -1,8 +0,0 @@
-import { Crevio } from "@crevio/sdk";
-
-export const crevio = new Crevio({
-	apiKey: process.env.CREVIO_API_KEY,
-	...(process.env.CREVIO_API_BASE_URL && {
-		serverURL: process.env.CREVIO_API_BASE_URL,
-	}),
-});

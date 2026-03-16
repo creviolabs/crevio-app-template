9e2e4e00aeca06b218b73bedd83d2b892498057d
Axel <73122370+axelgrubba@users.noreply.github.com>
2026-03-16 12:31:53 +0100

    feat: update origin in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,10 +18,10 @@ export default defineConfig(({ mode }) => ({
 		...crevioPlugins({
 			APP_ID: process.env.CREVIO_ACCOUNT_ID,
 			APP_URL: process.env.CREVIO_APP_URL,
-			ORIGIN: "*",
+			ORIGIN: process.env.CREVIO_APP_URL,
 			SHOW_WATERMARK: mode === "production",
 			NODE_ENV: mode,
-			DEBUG_LOGS: mode === "development" ? "true" : undefined,
+			DEBUG_LOGS: mode === "development" ? "true" : "false",
 			ANALYTICS_SCRIPT_ATTRS: JSON.stringify({
 				src: "https://static.crevio.co/analytics.js",
 				"data-domain": process.env.CREVIO_APP_URL,

c43e0c05153b7cbbd98ab5a69dfcc21cc38abb94
Axel <73122370+axelgrubba@users.noreply.github.com>
2026-03-23 12:03:26 +0100

    feat: added Content-Security-Policy headers

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,14 @@ const nextConfig: NextConfig = {
 	images: {
 		remotePatterns: [{ protocol: "https", hostname: "**" }],
 	},
+	headers: async () => [
+		{
+			source: "/:path*",
+			headers: [
+				{ key: "Content-Security-Policy", value: "frame-ancestors *" },
+			],
+		},
+	],
 };
 
 export default nextConfig;
